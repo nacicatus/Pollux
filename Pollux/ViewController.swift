4d49c4fa397e11e85011d8eb65f09070bcd128ee
nacicatus <drsaurabhsikka@gmail.com>
2020-09-01 22:49:47 +0200

    check for consent

--- Pollux/ViewController.swift
+++ Pollux/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import ResearchKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, ORKTaskViewControllerDelegate {
+    
+    var consented = false
+    
+    @IBAction func consentButtonClicked(_ sender: AnyObject)
+    {
+        let taskViewController = ORKTaskViewController(task: ConsentTask, taskRun: nil)
+        taskViewController.delegate = self
+        present(taskViewController, animated: true, completion: nil)
     }
-
-
+    
+    @IBAction func surveyButtonClicked(_ sender: AnyObject)
+    {
+        if consented == true
+        {
+            let taskViewController = ORKTaskViewController(task: SurveyTask, taskRun: nil)
+            taskViewController.delegate = self
+            present(taskViewController, animated: true, completion: nil)
+        } else {
+            print("Check consent first!")
+        }
+        
+        
+    }
+    
+    
+    
+    @IBAction func activeTaskButtonClicked(_ sender: AnyObject)
+    {
+        
+    }
+    
+    func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?)
+    {
+        switch reason {
+        case .completed:
+            consented = true
+        case .failed, .discarded, .saved:
+            consented = false
+        default:
+            consented = false
+        }
+        taskViewController.dismiss(animated: true, completion: nil)
+    }
 }
+
+
 
